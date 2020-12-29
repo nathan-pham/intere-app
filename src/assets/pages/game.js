cb816112becbd85e99f7e25b2e4d31790d589844
Nathan Pham <nathanpham.me@gmail.com>
2020-12-28 22:40:46 -0800

    canvas overlay

--- src/assets/pages/game.js
+++ src/assets/pages/game.js
@@ -1,29 +1,36 @@
+import { useState } from "react"
+
 import Person from "../components/game/classes/person.js"
 import * as config from "../components/game/config.js"
 
 import Canvas from "../components/canvas"
 
-const people = [ new Person(true) ]
-
-for(let i = 0; i < config.population; i++) {
-  people.push(new Person())
-}
-
-const draw = (ctx) => {
-    ctx.clearRect(0, 0, config.width, config.height)
-    people.forEach(person => {
-        person.collision(people)
-        person.update()
-        person.render(ctx)
-    })
-}
-
 const Game = () => {
+    const [ restart, setRestart ] = useState(false)
+
+    const people = [ new Person(true) ]
+
+    for(let i = 0; i < config.population; i++) {
+        people.push(new Person())
+    }
+
+    const draw = (ctx) => {
+        ctx.clearRect(0, 0, config.width, config.height)
+        people.forEach(person => {
+            person.collision(people)
+            person.update()
+            person.render(ctx)
+        })
+    }
+
     return (
         <div id="root-game">
             <Canvas draw={ draw } />
+            {/* <div className="canvas-overlay">
+
+            </div> */}
         </div>
     )
 }
 
-export default Game
+export default Game 
